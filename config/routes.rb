26411528ec575103f21b4625a7014263dbfaff2c
EC2 Default User <ec2-user@ip-172-31-30-127.ap-northeast-1.compute.internal>
2023-12-15 08:57:16 +0000

    管理者側　ログイン　ログアウト作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,54 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root to: "homes#top"
+  get '/homes/about', to:  'homes#about', as: 'about'
+  
+  resources :items, only: [:show, :index]
+  resources :registrations, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :customers, only: [:show, :edit, :update] do
+    collection do
+      get 'unsubscribe'
+      get 'withdraw'
+    end
+  end
+  resources :cart_items, only: [:index, :update] do
+    collection do
+      get 'destroy'
+      get 'deatroy_all'
+    end
+  end
+  resources :orders, only: [:new, :create, :index, :show] do
+    collection do
+      get 'confirm'
+      get 'thanks'
+    end
+  end
+  resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  
+  
+  namespace :admin do
+      resources :sessions, only: [:new, :create, :destroy]
+      resources :homes, only: [:top]
+      resources :items, only: [:show, :index, :new, :create, :edit, :update]
+      resources :genres, only: [:edit, :create,:index, :update]
+      
+      resources :customers, only: [:show, :edit, :update, :index] 
+      resources :orders, only: [:show, :update] 
+      resources :order_details, only: [:update] 
+      
+      end
+      
+  
 end
