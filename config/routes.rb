f0f588c429dc71791f84c15b0ceca4eedc65b0f7
mori618 <148190402+mori618@users.noreply.github.com>
2023-12-15 09:41:50 +0000

    森　商品一覧画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+  end
   devise_for :admins
   devise_for :customers
   devise_for :users
