a45adc74a5e7172550803945598ac16558fee168
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2023-12-16 02:24:41 +0000

    青井　商品一覧画面微調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
-  
+
   def index
     @items = item.paginate(page: params[:page], per_page: 8)
-    @posts = Post.all.page(params[:page]).per(8)
+
   end
 
   def show
