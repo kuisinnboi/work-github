e16af80609d4f500001aacbf06ca49421f413671
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2023-12-16 11:09:10 +0000

    青井　show画面仮完成

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  def add_tax_price
+        (self.price * 1.1).round
+  end
 end
