e3f65acbf597393ef9a36642e85c30dfdf20c656
mori618 <148190402+mori618@users.noreply.github.com>
2023-12-16 05:30:46 +0000

    森　商品詳細画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -8,9 +8,38 @@ class Admin::ItemsController < ApplicationController
     @genres = Genre.all
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path(@item), notice: "created successfully"
+    else
+      render 'new'
+    end
+  end
+
+
   def show
+    @item = Item.find(params[:id])
+
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
+  
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item), notice: "update successfully"
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:introduction,:is_active,:price)
+  end
+
 end
