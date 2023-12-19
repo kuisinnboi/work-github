64aa827e2b17eeb67917c233e3ed3142557b133d
EC2 Default User <ec2-user@ip-172-31-30-127.ap-northeast-1.compute.internal>
2023-12-19 07:43:34 +0000

    今門 注文情報確認画面 作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,9 +3,36 @@ class OrdersController < ApplicationController
 
 
       def new
+          @order = Order.new
       end
 
       def confirm
+          @order = Order.new(order_params)
+
+       @cart_items = CartItem.where(customer_id: current_customer.id)
+      @shipping_fee = 800
+      @cart_items_price = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
+      @total_price = @shipping_fee + @cart_items_price
+
+      @selected_pay_method = params[:order][:payment_method]
+      @address_type = params[:order][:address_type]
+      case @address_type
+      when "customer_address"
+        @selected_address = current_customer.post_code + " " + current_customer.address + " " + current_customer.last_name + current_member.first_name
+      when "registered_address"
+        unless params[:order][:registered_address_id] == ""
+          selected = Address.find(params[:order][:registered_address_id])
+          @selected_address = selected.post_code + " " + selected.address + " " + selected.name
+	 else
+	   render :new
+	 end
+      when "new_address"
+        unless params[:order][:new_post_code] == "" && params[:order][:new_address] == "" && params[:order][:new_name] == ""
+	  @selected_address = params[:order][:new_post_code] + " " + params[:order][:new_address] + " " + params[:order][:new_name]
+	else
+	  render :new
+	end
+      end
 
       end
 
@@ -14,7 +41,7 @@ class OrdersController < ApplicationController
       end
 
       def index
-        @orders = Order.where(member_id: current_member.id).order(created_at: :desc).
+        @orders = Order.where(customer_id: current_customer.id).order(created_at: :desc).
       end
 
       def show
@@ -25,6 +52,12 @@ class OrdersController < ApplicationController
       def complete
       end
    end
+
+   private
+   def order_params
+  params.require(:order).permit(:payment_method, :post_code, :address, :name)
+  end
+
   end
 
 
