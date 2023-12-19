017c43d659fae08906f8eaa872f801ce7664abeb
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2023-12-19 05:18:37 +0000

    青井　ログインエラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
 
+  devise_for :customers, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 
   get 'customers/show'
   get 'customers/edit'
@@ -17,17 +30,6 @@ Rails.application.routes.draw do
   namespace :admin do
     get 'homes/top'
   end
-  devise_for :customers, controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-  }
-
-  # 管理者用
-  # URL /admin/sign_in ...
-  devise_for :admin, controllers: {
-    sessions: "admin/sessions"
-  }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "homes#top"
   get '/homes/about', to:  'homes#about', as: 'about'
