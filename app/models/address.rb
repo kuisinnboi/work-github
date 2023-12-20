6a737ff78824f7e2e82786061b47596c97ea9719
EC2 Default User <ec2-user@ip-172-31-30-127.ap-northeast-1.compute.internal>
2023-12-20 02:51:05 +0000

    今門　注文、配送先作成

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,7 +2,7 @@ class Address < ApplicationRecord
   belongs_to :customer
 
   def address_display
-  '〒' + postal_code + ' ' + address + ' ' + name
+  '〒' + post_code + ' ' + address + ' ' + name
   end
 
 end
