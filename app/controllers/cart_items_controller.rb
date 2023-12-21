bd8a6b69b3ac03c999cec8dd912275fad72af856
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2023-12-21 05:22:23 +0000

    青井　エラー対応中

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -25,10 +25,12 @@ class CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = current_customer.cart_items.new(cart_item_params)
+    @cart_item = current_customer.cart_items.new(cart_items_params)
+    p 
       # もし元々カート内に「同じ商品」がある場合、「数量を追加」更新・保存する
       #ex.バナナ２個、バナナ２個ではなく　バナナ「4個」にしたい
       if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+        p 1
       #元々カート内にあるもの「item_id」
       #今追加した　　　　　　　params[:cart_item][:item_id])
         cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
@@ -39,10 +41,12 @@ class CartItemsController < ApplicationController
         redirect_to cart_items_path
         # もしカート内に「同じ」商品がない場合は通常の保存処理
       elsif @cart_item.save
+        p 2
             　@cart_items = current_customer.cart_items.all
-            　render 'index'
+            　redirect_to cart_items_path
       else　# 保存できなかった場合
-              render 'index'
+      p 3
+              redirect_to root_path
       end
   end
 
@@ -53,6 +57,6 @@ class CartItemsController < ApplicationController
   # end
 
   def cart_items_params
-    params.require(:cart_items).permit(:customer_id,:item_id,:amount)
+    params.require(:cart_item).permit(:customer_id,:item_id,:amount)
   end
 end
