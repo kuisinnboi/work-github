7ef8273879411a376c2df9632b01f7b92c27a565
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2023-12-21 06:15:30 +0000

    青井　カートエラー解消

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -3,7 +3,6 @@ class CartItemsController < ApplicationController
 
   def index
     @cart_items = current_customer.cart_items.all
-    @item = Item.find(params[:id])
     # @cart_item = current_customer.cart_item
     # @item = @cart_item.item
   end
@@ -21,31 +20,27 @@ class CartItemsController < ApplicationController
   def destroy_all
     cart_items = CartItem.all
     cart_items.destroy_all
-    　render 'index'
+      render 'index'
   end
 
   def create
     @cart_item = current_customer.cart_items.new(cart_items_params)
-    p 
       # もし元々カート内に「同じ商品」がある場合、「数量を追加」更新・保存する
-      #ex.バナナ２個、バナナ２個ではなく　バナナ「4個」にしたい
+      #ex.バナナ２個、バナナ２個ではなく  バナナ「4個」にしたい
       if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
-        p 1
       #元々カート内にあるもの「item_id」
-      #今追加した　　　　　　　params[:cart_item][:item_id])
+      #今追加した          params[:cart_item][:item_id])
         cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-        cart_item.quantity += params[:cart_item][:quantity].to_i
+        cart_item.amount += params[:cart_item][:amount].to_i
         #cart_item.quantityに今追加したparams[:cart_item][:quantity]を加える
                                                   #.to_iとして数字として扱う
         cart_item.save
         redirect_to cart_items_path
         # もしカート内に「同じ」商品がない場合は通常の保存処理
       elsif @cart_item.save
-        p 2
-            　@cart_items = current_customer.cart_items.all
-            　redirect_to cart_items_path
-      else　# 保存できなかった場合
-      p 3
+              @cart_items = current_customer.cart_items.all
+              redirect_to cart_items_path
+      else  # 保存できなかった場合
               redirect_to root_path
       end
   end
