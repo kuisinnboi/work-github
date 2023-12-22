190419edeb8ebebd006827a5789994793b9af69c
mori618 <148190402+mori618@users.noreply.github.com>
2023-12-22 08:16:10 +0000

    森　時間日本設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details
+  attribute :status, :integer, default: 0
 
   enum payment_method: { credit_card: 0, transfer: 1 },
        status: { awaiting_payment: 0, payment_confirmed: 1, in_production: 2, preparing_for_shipment: 3, shipped: 4 }
